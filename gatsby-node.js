a949f84e55f17b23f1e7a5771d2fbe24143511f1
SangWonKo <swko1017@naver.com>
2023-01-24 21:22:25 +0900

    chore: eslint 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,8 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+import path from 'path'
+import { createFilePath } from 'gatsby-source-filesystem'
 
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
   const output = getConfig().output || {}
