7c3a08470540b6edafdd90bf52ff4dd01e8ab4a2
SangWonKo <swko1017@naver.com>
2023-03-08 00:32:30 +0900

    feat: pwa 지원

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -69,17 +69,16 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: metaData.title,
+        short_name: metaData.title,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#0a87ef`,
         display: `minimal-ui`,
-        icon: `src/images/my_favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/my_favicon.png`,
       },
     },
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
